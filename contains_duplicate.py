86179adff239b0eb1081dba2109923acef6943be
guru <gururaj.br@dataweave.com>
2025-12-01 11:29:41 +0530

    contains_duplicate dsa

---
+++ contains_duplicate.py
@@ -0,0 +1,36 @@
+'''
+Contains Duplicate
+Given an integer array nums, return true if any value appears more than once in the array, otherwise return false.
+
+Example 1:
+
+Input: nums = [1, 2, 3, 3]
+
+Output: true
+
+Example 2:
+
+Input: nums = [1, 2, 3, 4]
+
+Output: false
+
+
+Recommended Time & Space Complexity
+You should aim for a solution with O(n) time and O(n) space, where n is the size of the input array.
+'''
+
+class Solution:
+    def hasDuplicate(self, nums):
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False
+
+# Example usage:
+solution = Solution()
+print(solution.hasDuplicate([1, 2, 3, 3]))  # Output: True
+print(solution.hasDuplicate([1, 2, 3, 4]))  # Output: False
+        
+
