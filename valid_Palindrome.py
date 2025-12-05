f93655a368401cc24508448584a86915d3b64dc3
guru <gururaj.br@dataweave.com>
2025-12-05 15:35:36 +0530

    valid_Palindrome

---
+++ valid_Palindrome.py
@@ -0,0 +1,42 @@
+'''
+Valid Palindrome
+Solved 
+Given a string s, return true if it is a palindrome, otherwise return false.
+
+A palindrome is a string that reads the same forward and backward. It is also case-insensitive and ignores all non-alphanumeric characters.
+
+Note: Alphanumeric characters consist of letters (A-Z, a-z) and numbers (0-9).
+
+Example 1:
+
+Input: s = "Was it a car or a cat I saw?"
+
+Output: true
+Explanation: After considering only alphanumerical characters we have "wasitacaroracatisaw", which is a palindrome.
+
+Example 2:
+
+Input: s = "tab a cat"
+
+Output: false
+Explanation: "tabacat" is not a palindrome.
+
+Constraints:
+
+1 <= s.length <= 1000
+s is made up of only printable ASCII characters.
+
+
+Recommended Time & Space Complexity
+You should aim for a solution with O(n) time and O(1) space, where n is the length of the input string.
+'''
+
+def is_palindrome(s: str):
+    cleaned = ''.join(char.lower() for char in s if char.isalnum())
+    return cleaned == cleaned[::-1]
+
+# Example usage:
+s1 = "Was it a car or a cat I saw?"
+print(is_palindrome(s1))  # Output: True
+s2 = "tab a cat"
+print(is_palindrome(s2))  # Output: False
